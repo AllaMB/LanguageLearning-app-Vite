00c73c93f0ab94a440b23f7f5abe886b860cafd0
AllaMB <126911591+AllaMB@users.noreply.github.com>
2023-11-26 18:51:11 +0100

    Add new functional components

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-    module.exports = {
-        presets: ["@babel/preset-env", "@babel/preset-react"],
-        plugins: [
-        ["react-css-modules", {
-            "generateScopedName": "[name]__[local]___[hash:base64:5]"
-        }]
-        ]
-    };
-    
