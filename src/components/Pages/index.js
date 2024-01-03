3b6ca308b1fc740e5ab03f38ead6cdb7a70ea85b
AllaMB <126911591+AllaMB@users.noreply.github.com>
2024-01-03 18:36:04 +0100

    Add state to track words learned during a session

--- src/components/Pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home/Home";
-import About from "./About/About";
-import ExploreWords from "./ExploreWords/ExploreWords"
-import GamePage from "./GamePage/GamePage";
-import NotFoundPage from "../NotFoundPage/NotFoundPage";
-
-export {Home, About, ExploreWords, GamePage, NotFoundPage}
